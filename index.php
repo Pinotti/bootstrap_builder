c1ac0524caa9e6965a0c8a549199ac9c5d24733a
Anderson <anderson.pinotti@gmail.com>
2022-08-05 20:24:39 -0300

    feat: Iniciando projeto prático

--- index.php
+++ index.php
@@ -6,11 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bootstrap Builder</title>
 
-    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
-
+    <link rel="stylesheet" href="_cdn/css/bootstrap_custom.css">
+    <link rel="stylesheet" href="_cdn/css/app.css">
 </head>
 <body>
     <h1>TESTE</h1>
+
+    <button class="btn btn-front icon-info"></button>
     
     <script src="_cdn/js/jquery-3.6.0.min.js"></script>
     <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
