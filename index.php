4494d872e8a4768a30089a3c3c6dbedd2955e6cc
Anderson <anderson.pinotti@gmail.com>
2022-08-05 18:34:31 -0300

    feat: Instalação do Bootstrap e do jQuery

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap Builder</title>
+
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+
+</head>
+<body>
+    <h1>TESTE</h1>
+    
+    <script src="_cdn/js/jquery-3.6.0.min.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
